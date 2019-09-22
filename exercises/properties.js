374082cbd81b827a9a2ae9fae56c1f76d0dfe1d7
szczepaniakmichal <36078683+szczepaniakmichal@users.noreply.github.com>
2019-09-22 14:17:11 +0200

    undefined

--- exercises/properties.js
+++ exercises/properties.js
@@ -3,14 +3,22 @@ const extraPropertiesName = 'isEmployee';
 const person = {
     firstName: 'Jan',
     lastName: 'Nowak',
+    email: undefined,
     [extraPropertiesName]: false,
 };
 
 //person[extraPropertiesName] = true; // dynamiczne tworzenie properties
 person.age = 70;
 person['place of birth'] = 'Warszawa'; // to nie jest dobra praktyka
+person.companyName;
 
 console.log('person', person);
+
+console.log('person.companyName', person.companyName);
+console.log(person.hasOwnProperty("companyName"), person.hasOwnProperty('companyName') );
+
+console.log('person.email', person.email);
+console.log(person.hasOwnProperty("email"), person.hasOwnProperty('email') );
 
 
 
